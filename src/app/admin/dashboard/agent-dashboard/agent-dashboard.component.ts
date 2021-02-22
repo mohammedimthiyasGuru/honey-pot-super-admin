4c9ad89a7440f38246b68e460dca8aa9bca44995
VIGNESH V <vignesh.vedhamoorthy07@gmail.com>
2021-02-22 19:08:35 +0530

    code-push

--- src/app/admin/dashboard/agent-dashboard/agent-dashboard.component.ts
+++ src/app/admin/dashboard/agent-dashboard/agent-dashboard.component.ts
@@ -1,16 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
+import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../../api.service';
 @Component({
   selector: 'app-agent-dashboard',
   templateUrl: './agent-dashboard.component.html',
   styleUrls: ['./agent-dashboard.component.css']
 })
 export class AgentDashboardComponent implements OnInit {
-
-  constructor() { }
+  client_name: any;
+  business_divi: any;
+  product_name: any;
+  profolio: any;
+  type: any;
+  rows: any;
+  searchQR: any;
+  id: any;
+  edit_f: boolean = false;
+  client_list: any = [];
+  product_list: any = [];
+  profolio_list: any = [];
+  constructor(
+    private router: Router,
+    private _api: ApiService,
+    @Inject(SESSION_STORAGE) private storage: StorageService,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit(): void {
     window.scrollTo(0, 0);
+
+    this._api.portfolio_type_list().subscribe(
+      (response: any) => {
+        console.log(response);
+        let list = response.Data.reverse();
+        for (let i = 0; i < list.length; i++) {
+          let obj = { 'y': list[i].portfolio_type };
+          this.profolio_list.push(obj);
+        }
+
+        console.log(this.profolio_list);
+      }
+    );
   }
 
 }
